5f6858745d2a3c171f0a51ad6678bb0382dc40bd
Omar Yahia <therealrandomath@gmail.com>
2024-12-30 09:50:02 -0800

    added instructions on how to use repo / fixed some small bugs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wordhunt Solver
+## Credits
+- words.txt is from [dwyl/english-words](https://github.com/dwyl/english-words)
+- words2.txt is from [matthewreagan/WebstersEnglishDictionary](https://github.com/matthewreagan/WebstersEnglishDictionary)
+  - to create the txt file, I extracted all the keys from the `dictionary.json` file
+- words3.txt is the same as words2.txt but with the added words from `update_words.py` which adds the plural forms of words
+## How to use
+1. Change the code and files specified in Line 7 of `cheat.py`
+2. Make a new python 3.9 environment and install `requirements.txt`
+2. Run python `cheat.py`
+3. If you need to stop the program, move the mouse to the top left corner of the screen, which acts as a failsafe for `pyautogui`
--- cheat.py
+++ cheat.py
@@ -1,11 +1,11 @@
-THEME = "Just the essentials"
+WORDS_FILE = "words3.txt"
 
 import pyautogui
 from tqdm import tqdm
 import time
-from llm import generate
-import pickle
-import os
+
+print('TO USE PLEASE SET TOP LEFT AND MUL ON LINES 74-75, CHANGE GRID.TXT, AND DELETE THE EXIT() on LINE 8')
+exit()
 
 grid = []
 letters = []
@@ -38,7 +38,7 @@ def check_valid(r, c, val, word, visited: list):
     if val != word[0]:
         return []
     visited.append((r, c))
-    if len(word) == 1: 
+    if len(word) == 1:
         return visited
     for rnew, cnew, valnew in adjacency_list[r, c]:
         if (rnew, cnew) not in visited:
@@ -60,65 +60,36 @@ def valid_word(word):
             return path
     return []
 
-with open('words.txt', 'r') as f:
+with open(WORDS_FILE, 'r') as f:
     n = len(f.readlines())
 
 paths = {}
 
-with open('words3.txt', 'r') as f:
+with open(WORDS_FILE, 'r') as f:
     for line in tqdm(f, desc='trying words', total=n):
         path = valid_word(line.strip())
         if path:
             paths[line.strip()] = path
 
-words = [word for word in paths]
-
-# genmodel
-files = os.listdir('themes')
-if THEME in files:
-    print('loading previous openai response')
-    with open(f'themes/{THEME}', 'rb') as f:
-        generated = pickle.load(f)
-else:
-    print('dumping new openai response')
-    sys_prompt = f"""
-    Given the following theme: {THEME}
-    You will be instructed to pick the most likely words that fit with the theme
-    Generate the words and only the words with a space separating each word
-    """
-    user_prompt = f"""
-    Pick the most likely words associated with the theme using the following word list:
-    {words}
-    """
-    with open(f'themes/{THEME}', 'wb') as f:
-        generated = generate(sys_prompt, user_prompt)
-        pickle.dump(generated, f)
-    
-new_words = generated.choices[0].message.content.split()
-to_do = []
-for word in new_words:
-    if word in paths:
-        to_do.append(paths[word])
-for key in sorted(paths.keys() - set(new_words)):
-    to_do.append(paths[key])
-
-topleft = (630, 570)
-mul = 42
+TOPLEFT = (1040, 420) # x and y coordinate of grid[0][0] on the screen
+MUL = 60 # how much distance between each grid element on the screen
 
 def get_screen_c(x):
-    return topleft[0] + mul*x
+    return TOPLEFT[0] + MUL*x
 def get_screen_r(y):
-    return topleft[1] + mul*y
+    return TOPLEFT[1] + MUL*y
 def moveTo(r, c, delay=0.0):
-    pyautogui.moveTo(get_screen_c(c), get_screen_r(r), duration=delay)
+    pyautogui.dragTo(get_screen_c(c), get_screen_r(r), button='left', mouseDownUp=False, duration=delay)
 
 print('STARTING IN 5')
 time.sleep(5)
 
-MIN_DELAY = 0
+MIN_DELAY = 0 # change if you want it slower
 
-for path in to_do:
-    for r, c in path:
+for word, path in sorted(paths.items(), key=lambda x: len(x[0]), reverse=True):
+    rstart, cstart = path[0]
+    moveTo(rstart, cstart, delay=MIN_DELAY)
+    pyautogui.mouseDown()
+    for r, c in path[1:]:
         moveTo(r, c, delay=MIN_DELAY)
-        pyautogui.click()
-    pyautogui.click()
+    pyautogui.mouseUp()
